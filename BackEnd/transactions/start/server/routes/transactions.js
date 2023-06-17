dae2eb2ef2395db1b85f2847c06143a891033769
Lwin Paing Oo <78170253+Lwin-Oo@users.noreply.github.com>
2023-06-16 22:28:01 -0500

    Update transactions.js

--- BackEnd/transactions/start/server/routes/transactions.js
+++ BackEnd/transactions/start/server/routes/transactions.js
@@ -15,12 +15,42 @@ const router = express.Router();
  */
 router.post("/sync", async (req, res, next) => {
   try {
-    res.json({ todo: "Implement this method" });
+    const userId = getLoggedInUserId(req);
+    const items = await db.getItemIdsForUser(userId);
+    items.forEach((item) =>{
+        syncTransactions(item.id);
+    })
+
   } catch (error) {
     console.log(`Running into an error!`);
     next(error);
   }
 });
+
+const fetchNewSyncData = async function (accessToken){
+  let keepGoing = false;
+  const allData = { added: [], modified: [], removed: [], nextCursor: "CAESJUVYTDRSVjZBQnlGZ0c0ZzRYelF2aUd4Umw2azk2d1Q0QXJLVzYaBgiAlqmkBiIGCICWqaQGKgYIgJappAY=" };
+  do{
+    const results = await plaidClient.transactionsSync({
+      access_token: accessToken,
+      //A new option obj added here.
+      cursor: allData.nextCursor,
+      options:{
+       include_personal_finance_category: true,
+      },
+   });
+   const newData = results.data;
+   allData.added = allData.added.concat(newData.added);
+   allData.modified = allData.modified.concat(newData.modified);
+   allData.removed = allData.removed.concat(newData.removed);
+   allData.nextCursor = newData.next_cursor;
+   keepGoing = newData.has_more;
+   console.log(`Added: ${newData.added.length} Modified: ${newData.modified.length} Removed: ${newData.removed.length}`);
+  }while (keepGoing == true)
+  console.log('All done!');
+  console.log(`Your final cursor: ${allData.nextCursor}`);
+  return allData;
+};
 
 /**
  * Given an item ID, this will fetch all transactions for all accounts
@@ -30,7 +60,20 @@ router.post("/sync", async (req, res, next) => {
  */
 const syncTransactions = async function (itemId) {
   const summary = { added: 0, removed: 0, modified: 0 };
-  // TODO: Implement this!
+  const {access_token : accessToken}= await db.getItemInfo(itemId);
+  //1. Fetch our most recent cursor from the db.
+
+  //2. Fetch all our transactions since last cursor
+
+  const allData = await fetchNewSyncData(accessToken);
+  //3. Add new transactions to our db.
+
+  //4. Updated any modified transactions
+
+  //5. Do sth with removed transactions??
+
+  //6. Save our recent cursor.
+
   return summary;
 };
 
